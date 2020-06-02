50f93da008ee9f0b5a36d79c32ec88b1d9bd7575
AaditT <aadit.trivedi03@gmail.com>
2020-06-01 19:45:25 -0700

    Initial Commit

---
+++ ColorSplash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorSplash
+//
+//  Created by Aadit Trivedi on 6/1/20.
+//  Copyright © 2020 Aadit Trivedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
